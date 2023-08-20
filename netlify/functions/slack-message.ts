3ec5952a07c3785e7f88f8df3791fbfc1633bfde
Maksym Brazhnyk <maksym.brazhnyk@weblabs.pro>
2023-08-20 18:54:27 +0300

    test slack bot

--- netlify/functions/slack-message.ts
+++ netlify/functions/slack-message.ts
@@ -4,11 +4,6 @@ import slackBot from "../../servises/slack-service";
 import SlackController from "../../controllers/slack-controller";
 import { FIRED_TEXT_MATCH_REGEX } from "../../consts";
 
-slackBot.message(
-  FIRED_TEXT_MATCH_REGEX,
-  SlackController.onMessage.bind(SlackController),
-);
-
 const handler: Handler = async (event: HandlerEvent) => {
   const payload = JSON.parse(event.body!) as ReceiverEvent;
 
@@ -24,6 +19,11 @@ const handler: Handler = async (event: HandlerEvent) => {
       });
     },
   };
+
+  slackBot.message(
+    FIRED_TEXT_MATCH_REGEX,
+    SlackController.onMessage.bind(SlackController),
+  );
 
   await slackBot.processEvent(slackEvent);
 
