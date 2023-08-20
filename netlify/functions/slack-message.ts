6a4d9ae82f2b9f970fd045237311c032ed78c9ce
Maksym Brazhnyk <maksym.brazhnyk@weblabs.pro>
2023-08-20 17:45:20 +0300

    set slack endpoint

--- netlify/functions/slack-message.ts
+++ netlify/functions/slack-message.ts
@@ -5,11 +5,17 @@ import { Handler, HandlerEvent } from "@netlify/functions";
 const handler: Handler = async (event: HandlerEvent) => {
   console.log(event.body);
   const message = JSON.parse(event.body!);
+  console.log(message?.challenge);
 
   // await telegramController.onAction.bind(telegramController)(message);
 
   // @ts-ignore
-  return message?.challenge;
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      challenge: message?.challenge,
+    }),
+  };
 };
 
 export { handler };
