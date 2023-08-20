e408404cfc6212cf12d4020ec7bd3a6ba42f8bd7
Maksym Brazhnyk <maksym.brazhnyk@weblabs.pro>
2023-08-20 19:09:28 +0300

    test slack bot

--- servises/slack-service.ts
+++ servises/slack-service.ts
@@ -1,11 +1,17 @@
-import { App } from "@slack/bolt";
+import { App, ExpressReceiver } from "@slack/bolt";
 import { config } from "dotenv";
 
 config();
 
+const expressReceiver = new ExpressReceiver({
+  signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
+  processBeforeResponse: true,
+});
+
 const bot = new App({
   token: process.env.SLACK_BOT_TOKEN as string,
   signingSecret: process.env.SLACK_SIGNING_SECRET as string,
+  receiver: expressReceiver,
 });
 
 export default bot;
